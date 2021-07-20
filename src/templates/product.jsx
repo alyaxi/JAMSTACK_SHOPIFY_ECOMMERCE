954cf99ec9a735e0b274609548f7ee5b6090855a
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-07-21 03:53:15 +0500

    template file updated

--- src/templates/product.jsx
+++ src/templates/product.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 export default function product({pageContext}) {
-    console.log(pageContext);
+    const {priceRange : {maxVariantPrice : {amount}}, title, description, images} = pageContext
     return (
         <div>
-            
+            <img src={images[0].originalSrc} alt="" />
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <p><b>Price:</b> {amount}</p>
         </div>
     )
 }
